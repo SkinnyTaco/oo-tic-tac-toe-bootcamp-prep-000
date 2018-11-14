4dd084ed77b6281c75ba6f3c35e7e6aef2711744
Daniel Dawson <dannyldawson@gmail.com>
2018-11-14 22:38:48 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -50,5 +50,45 @@ end
 def current_player
   turn_count % 2 == 1 ? "O" : "X"
 end
+
+def won?
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+    
+    position_1 = @board[win_index_1]
+    position_2 = @board[win_index_2]
+    position_3 = @board[win_index_3]
+    
+    if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
+      return win_combination
+    else 
+      false
+    end
+  end 
+  false
+end
+
+def full?
+  @board.reject{|i| i == " "}.length ==9
+end
+
+def draw?
+  if !won? && full?
+    return true
+  end
+  false
+end
+
+def over?
+  if won? || draw? || full?
+    true
+  end
+end
+
+def winner
+  won? ? @board[won?[0]] : nil
+end
   
 end
